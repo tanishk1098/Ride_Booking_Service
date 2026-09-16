ede66fb8f868b66b42f3112246d74a6ad9e69293
tanishk1098 <tanishk.singh1098@gmail.com>
2026-09-16 16:14:25 +0530

    Update ingest.py

--- Code_Files/ingest.py
+++ Code_Files/ingest.py
@@ -1,14 +1,18 @@
 from pyspark import pipelines as dp
-from pyspark.sql.functions import *
 from pyspark.sql.types import *
-
+from pyspark.sql.functions import *
 
 # Event Hubs configuration
-EH_NAMESPACE  = "uberevents"
-EH_NAME = "ubertopic"
+EH_NAMESPACE = "UberEventsTani"
+EH_NAME = "ubertopictani"
+#Method 1 : configuration settings in the spark session
+#Method 2: using databricks secrets using databricsk CLI commands
+#method 3: dorectly using the connection string in the code (not recommended for production)
 
+EH_CONN_STR = spark.conf.get("connection_string") #connection_string variable is stred in configuration!!
+#EH_CONN_STR = dbutils.secrets.get(scope="ride-booking-secrets", key="connection_string")
 
-EH_CONN_STR  = spark.conf.get("connection_string")
+# Kafka Consumer configuration
 
 KAFKA_OPTIONS = {
   "kafka.bootstrap.servers"  : f"{EH_NAMESPACE}.servicebus.windows.net:9093",
@@ -23,20 +27,11 @@ KAFKA_OPTIONS = {
   "startingOffsets"          : 'earliest'
 }
 
+#CREATED A STREAMING TABLE "rides_raw"  into uber catalog and bronze schema as defined in the configuration settings !!
 @dp.table
 def rides_raw():
-    df = spark.readStream.format("kafka")\
-                .options(**KAFKA_OPTIONS)\
-                .load()
-
-    # Converting Values To string
-    df = df.withColumn("rides",col("value").cast("string"))
-
-    return df
-
-
-
-
-
-
-
+ df=spark.readStream.format('kafka')\
+  .options(**KAFKA_OPTIONS)\
+  .load()
+ df=df.withColumn("rides",col("value").cast(StringType()))
+ return df
